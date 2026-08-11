2526d4c3188b8ba3ed58b30a837813e2052d1243
Cristian Czika <cristianczika6@gmail.com>
2026-08-11 11:57:51 +0300

    feat: add flyway database migration

--- Backend/src/main/java/com/example/CheckInApp/model/User.java
+++ Backend/src/main/java/com/example/CheckInApp/model/User.java
@@ -1,23 +1,12 @@
-package main.java.com.example.CheckInApp.model;
+package com.example.CheckInApp.model;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 import lombok.EqualsAndHashCode;
@@ -50,5 +39,5 @@ public class User {
     @Enumerated(EnumType.STRING) 
     @Column(name = "role_name")
     private Set<Role> roles = new HashSet<>(); 
-    }
+}
 
