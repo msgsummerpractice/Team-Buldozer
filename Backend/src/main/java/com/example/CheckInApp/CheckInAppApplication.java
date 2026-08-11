221272f50bfb44b203181b62d774156edd9eccb7
Ioana Marica <ioanamarica66@gmail.com>
2026-08-11 09:03:23 +0200

    TEAMB-112 Add initial backend implementation

---
+++ Backend/src/main/java/com/example/CheckInApp/CheckInAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.CheckInApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckInAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckInAppApplication.class, args);
+	}
+
+}
