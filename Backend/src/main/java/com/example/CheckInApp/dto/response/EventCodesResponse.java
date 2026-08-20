0ba67c53be9692e75ba44c8ca423e66ed4ecf487
ioan alexandru <ionutpuiu010405@gmail.com>
2026-08-20 09:16:55 +0200

    Revert "BugFix/Refactoring:TeamB-159: Explain details about qrcode, Remove unused spaces, Change redundant if statement"
    This reverts commit cd9b959a46514373dd97982a073eef0a1660de62.

--- Backend/src/main/java/com/example/CheckInApp/dto/response/EventCodesResponse.java
+++ Backend/src/main/java/com/example/CheckInApp/dto/response/EventCodesResponse.java
@@ -1,9 +1,12 @@
 package com.example.CheckInApp.dto.response;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+
 
 @Data
 @AllArgsConstructor
--- Backend/src/main/java/com/example/CheckInApp/service/EventService.java
+++ Backend/src/main/java/com/example/CheckInApp/service/EventService.java
@@ -300,8 +300,12 @@ public class EventService {
             throw new InvalidEventDataException("Codes are only available for published events.");
         }
 
-        if (event.getCheckInCode() == null || event.getQrCode() == null) {
+        if (event.getCheckInCode() == null) {
             throw new ResourceNotFoundException("Check-in code has not been generated yet for event with id " + eventId);
+        }
+
+        if (event.getQrCode() == null) {
+            throw new ResourceNotFoundException("QR code has not been generated yet for event with id " + eventId);
         }
 
         return new EventCodesResponse(event.getCheckInCode(), Base64.getEncoder().encodeToString(event.getQrCode()));
