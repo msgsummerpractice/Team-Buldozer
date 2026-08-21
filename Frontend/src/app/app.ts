165123394e9d2240fc4066fff3016bde88716b0c
Laura Muresan <laura.muresan@msg.group>
2026-08-21 10:21:59 +0300

    refactor app.ts and app.html

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -14,6 +14,6 @@ export class App {
   private readonly loadingService = inject(LoadingService);
 
   protected get isLoading() {
-    return this.loadingService.isLoading();
+    return this.loadingService.isLoading;
   }
 }
