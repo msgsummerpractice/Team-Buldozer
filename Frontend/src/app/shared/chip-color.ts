abf647176e143be091cb18b66083f7a3ca72ecd6
Cristian-Alexandru Czika <cristianczika6@gmail.com>
2026-08-24 16:17:53 +0300

    fix: users table mobile view (#75)

--- Frontend/src/app/shared/chip-color.ts
+++ Frontend/src/app/shared/chip-color.ts
@@ -1,9 +1,11 @@
-import { UserRoleEnum } from '@core/users/model/user-role';
+import { UserRole, UserRoleEnum } from '@core/users/model/user-role';
 import { EventStatus, EventStatusEnum } from '@features/events/model/event-status';
 import { EventType, EventTypeEnum } from '@features/events/model/event-type';
 import { EventLocation, EventLocationEnum } from '@features/events/model/event-location';
 
-export const getChipColor = (enumKey: UserRoleEnum | EventStatus | EventStatusEnum): string => {
+export const getChipColor = (
+  enumKey: UserRole | UserRoleEnum | EventStatus | EventStatusEnum
+): string => {
   const colors: Record<UserRoleEnum | EventStatusEnum, string> = {
     ADMIN: 'var(--color-msg-orange)',
     HR: 'var(--color-msg-purple)',
