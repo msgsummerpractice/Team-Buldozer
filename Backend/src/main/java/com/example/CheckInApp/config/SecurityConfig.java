c966b81fe7219c4491fc3b498ca4b7b65aff614b
Laura Mureșan <lauramuresan420@gmail.com>
2026-08-25 12:18:17 +0300

    Bugfix(teamb) login guard (#83)

--- Backend/src/main/java/com/example/CheckInApp/config/SecurityConfig.java
+++ Backend/src/main/java/com/example/CheckInApp/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://ashy-glacier-059589b03.7.azurestaticapps.net"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
