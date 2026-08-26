7c35ce8bc89906f612f6aebe443a590fd78b9ed9
Laura Muresan <laura.muresan@msg.group>
2026-08-26 11:05:43 +0300

    fix query to be more readable

--- Backend/src/main/java/com/example/CheckInApp/repository/RegistrationRepository.java
+++ Backend/src/main/java/com/example/CheckInApp/repository/RegistrationRepository.java
@@ -13,7 +13,10 @@ public interface RegistrationRepository extends JpaRepository<Registration, Long
 
     boolean existsByEventIdAndUserId(Long eventId, Long userId);
 
-    @Query("SELECT r FROM Registration r JOIN FETCH r.user WHERE r.event.id = :eventId ORDER BY r.id")
+    @Query("""
+            SELECT r
+            FROM Registration r JOIN FETCH r.user WHERE r.event.id = :eventId ORDER BY r.id
+            """)
     List<Registration> findAllByEventIdWithUser(@Param("eventId") Long eventId);
 
 }
