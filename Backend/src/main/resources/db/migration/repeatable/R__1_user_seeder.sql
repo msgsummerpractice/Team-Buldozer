5deaf2b6d9eb6c2ff51fa4db4a255a6584fefc51
Cristian Czika <cristianczika6@gmail.com>
2026-08-27 12:38:03 +0300

    refactor seeders

--- Backend/src/main/resources/db/migration/repeatable/R__1_user_seeder.sql
+++ Backend/src/main/resources/db/migration/repeatable/R__1_user_seeder.sql
@@ -1,26 +1,19 @@
 INSERT INTO users (id, first_name, last_name, email, password, location, status, profile_picture)
 VALUES (1, 'Admin', 'Admin', 'admin@gmail.com', '$2a$12$x/NuM.sSIYNII6GnGh1is.89PgjPZckmfHGyBuOGy6Q.W6Xt8HLza',
         'CLUJ', true, NULL),
-       (2, 'Ion', 'Popescu', 'ion@gmail.com', '$2a$12$oc1CZXMNywk4ZzHISZLmqumpHXjnFBhuWxz7tZhvTnb86E1k4AUeG',
-        'TIMISOARA', true, NULL),
-       (3, 'Maria', 'Ionescu', 'maria@gmail.com', '$2a$12$pJNI92kSHeqIiNeL6T3XKO.FEKCy7ql4TQ5YQ93mOifOgPKUrzlze',
-        'TIMISOARA', true, NULL),
-       (4, 'Andrei', 'Muresan', 'andrei@gmail.com', '$2a$12$BMNTEt0HP4x1OPwfqW94COKIfwXzPHtbKQTTNm2RgY9rLywfNAWcW',
+       (2, 'Marketing', 'Marketing', 'marketing@gmail.com',
+        '$2a$12$Dc1p0yxuPaZ4uGU52H7VUOIG4TMZcDmGbiZIdKFKaWSaEv7i9i8dC',
         'CLUJ', true, NULL),
-       (5, 'George', 'Dobre', 'george@gmail.com', '$2a$12$nco8Kg9qDJhieMpHdNBuRebcNfuQFiUr6fqQC8iKh7iV6KZZQt78C',
+       (3, 'Hr', 'Hr', 'hr@gmail.com', '$2a$12$wTYjjZyby8DmavhGjBk8O.fdge0IyD5HAMFUTcb0wqhGyoK3unjC2',
+        'CLUJ', true, NULL),
+       (4, 'Ioan', 'Puiu', 'ionutpuiu010405@gmail.com', '$2a$12$.h6Z1/tpkOv0dnfdZBgyVeTKKAB8uASI2PFZWQZ4X5GQwU70g04..',
         'MURES', true, NULL),
-       (6, 'Elena', 'Radu', 'elena@gmail.com', '$2a$12$oc1CZXMNywk4ZzHISZLmqumpHXjnFBhuWxz7tZhvTnb86E1k4AUeG',
+       (5, 'Laura', 'Muresan', 'lauramuresan420@gmail.com',
+        '$2a$12$.h6Z1/tpkOv0dnfdZBgyVeTKKAB8uASI2PFZWQZ4X5GQwU70g04..', 'TIMISOARA', true, NULL),
+       (6, 'Ioana', 'Marica', 'ioanamarica66@gmail.com', '$2a$12$.h6Z1/tpkOv0dnfdZBgyVeTKKAB8uASI2PFZWQZ4X5GQwU70g04..',
         'CLUJ', true, NULL),
-       (7, 'Diana', 'Stan', 'diana@gmail.com', '$2a$12$nco8Kg9qDJhieMpHdNBuRebcNfuQFiUr6fqQC8iKh7iV6KZZQt78C',
-        'CLUJ', true, NULL),
-       (8, 'Mihai', 'Vasile', 'mihai@gmail.com', '$2a$12$nco8Kg9qDJhieMpHdNBuRebcNfuQFiUr6fqQC8iKh7iV6KZZQt78C',
-        'CLUJ', true, NULL),
-       (9, 'Ioana', 'Barbu', 'ioana@gmail.com', '$2a$12$nco8Kg9qDJhieMpHdNBuRebcNfuQFiUr6fqQC8iKh7iV6KZZQt78C',
-        'TIMISOARA', true, NULL),
-       (10, 'Radu', 'Constantin', 'radu@gmail.com', '$2a$12$nco8Kg9qDJhieMpHdNBuRebcNfuQFiUr6fqQC8iKh7iV6KZZQt78C',
-        'TIMISOARA', true, NULL),
-       (11, 'Cristina', 'Enache', 'cristina@gmail.com', '$2a$12$nco8Kg9qDJhieMpHdNBuRebcNfuQFiUr6fqQC8iKh7iV6KZZQt78C',
-        'MURES', true, NULL)
+       (7, 'Cristi', 'Czika', 'cristiczika@gmail.com', '$2a$12$.h6Z1/tpkOv0dnfdZBgyVeTKKAB8uASI2PFZWQZ4X5GQwU70g04..',
+        'CLUJ', true, NULL)
 ON CONFLICT (id) DO NOTHING;
 
 SELECT setval(pg_get_serial_sequence('users', 'id'), coalesce(max(id), 1))
@@ -32,12 +25,8 @@ VALUES (1, 'ADMIN'),
        (3, 'HR'),
        (4, 'PARTICIPANT'),
        (5, 'PARTICIPANT'),
-       (6, 'MARKETING'),
-       (7, 'PARTICIPANT'),
-       (8, 'PARTICIPANT'),
-       (9, 'PARTICIPANT'),
-       (10, 'PARTICIPANT'),
-       (11, 'PARTICIPANT')
+       (6, 'PARTICIPANT'),
+       (7, 'PARTICIPANT')
 ON CONFLICT (user_id, role_name) DO NOTHING;
 
 
